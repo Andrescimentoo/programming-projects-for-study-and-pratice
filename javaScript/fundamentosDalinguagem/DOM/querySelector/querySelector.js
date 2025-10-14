e4ca4cf4a999860b17b31e9bc73f9752681ffbfc
Andre Nascimento <1914andre@gmail.com>
2025-10-13 22:34:36 -0300

    feat: adding logic Js to challenge querySelector

---
+++ javaScript/fundamentosDalinguagem/DOM/querySelector/querySelector.js
@@ -0,0 +1,7 @@
+
+const element = document.querySelector(".H-teste")
+console.log(element)
+
+// perceba o querySelector traz elementos assim como getElementbyId e etc... MAAAASSS ele sempre vai trazer o primeiro item com essa class
+// ou o item com o primeiro elemnto, exemplo facil como esse, mais de uma tag tem a mesma class e as duas estão sendo chamadas nesse método
+// porem se você notar a saída do console, berá que o querySelector só mostra um dos itens em tela, adivinha qual? O PRIMEIRO   
