6e06594fd5ac1901770f68ca4b2ac4eac18dfd06
Andre Nascimento <1914andre@gmail.com>
2025-10-13 22:30:55 -0300

    feat: adding logic Js to challenge getElementByClassName

---
+++ javaScript/fundamentosDalinguagem/DOM/getElementByClassName/getElementByClassName.js
@@ -0,0 +1,6 @@
+const elementClass = document.getElementsByClassName('classe-teste') 
+console.log(elementClass)
+
+// Explicando, esse método getElementByClassName trás todas as classes que tiverem o nome classe-teste
+// isso ajuda quando queremos pegar mais de um elemnto mas que talvez não faça parte de uma mesma class, é uma forma específica de usar
+// nem tudo a gente usa sempre ou até chega a usar, mas o bom é saber que existe , vai que um dia precisa.....
