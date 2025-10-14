f419e53f583c74c02d920621088a2bac65f8da4e
Andre Nascimento <1914andre@gmail.com>
2025-10-13 22:35:48 -0300

    feat: adding logic js to challenge querySelectorAll

---
+++ javaScript/fundamentosDalinguagem/DOM/querySelectorAll/querySelectorAll.js
@@ -0,0 +1,5 @@
+
+const allElements = document.querySelectorAll(".main-container")
+console.log(allElements)
+
+// sem nove horas. O querySelectorAll trás todo mundo dentro do elemnto selecionado, ou seja tudo o que mostrarmso a ele ele trará
