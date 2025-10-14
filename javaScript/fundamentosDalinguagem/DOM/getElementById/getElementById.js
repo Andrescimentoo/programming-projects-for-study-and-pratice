c741099fe253d60aa333431b27ff6ef941d3c682
Andre Nascimento <1914andre@gmail.com>
2025-10-13 22:32:19 -0300

    feat: adding logic Js to challenge getElementByid

---
+++ javaScript/fundamentosDalinguagem/DOM/getElementById/getElementById.js
@@ -0,0 +1,6 @@
+
+
+const pegarTagPeloID = document.getElementById('h1') 
+// O que eu aprendi? primeiro através do document. eu literlamente estou pegando todo o documento web.logo em seguida uso um método que me 
+// permite pegar um ou mais elementos específicos desse documento.
+console.log(pegarTagPeloID)
