90a06d1b0073bf13467abc527b1a48d1d39c2655
Andre Nascimento <1914andre@gmail.com>
2025-10-13 22:29:26 -0300

    feat: adding logic Js to desafio rapido DOM

---
+++ javaScript/fundamentosDalinguagem/DOM/desafioRapidoDOM/desafioRapidoDOM.js
@@ -0,0 +1,10 @@
+
+const tag = document.querySelector('.paragrafo')
+
+const adicionarTexto = tag.innerHTML = 'olá eu sou o texto desse paragrafo '
+
+console.log(adicionarTexto)
+
+// peguei um paragrafo vazio e atraves da propriedade InnerHTML eu adicionei um texto. Memso processo, pega o elemnto pelo seletor
+// e executa a cação pela propriedade.
+
